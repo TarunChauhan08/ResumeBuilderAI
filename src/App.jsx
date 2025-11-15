cb581d29cb8fc916bf0f0addf55a6cebeac6cc04
“[Tarun <[tarunchauhan199908@gmail.com]>
2025-11-16 02:54:40 +0530

    dashboard page done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Layout from "./pages/Layout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Preview from "./pages/Preview.jsx";
+import ResumeBuilder from "./pages/ResumeBuilder.jsx";
+import Login from "./pages/Login.jsx";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+        <Route path="view/:resumeId" element={<Preview />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
